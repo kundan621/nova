5aa6a43acc5d6109265c174963e0a0690b804d70
Kundan Kumar <kundan621@gmail.com>
2024-01-11 02:39:14 +0530

    home page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,19 @@
 const Home = () => {
-  return ( "Home" );
+  return ( 
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        HK Software
+        <br className="max-md:hidden"/>
+        <span className="orange_gradient text-center"> Consulting pro Max</span>
+      </h1>
+
+      <p className="desc text-center">
+        HK Software is a product maker company. We make products which brings fortune and helps people solve their problems.
+      </p>
+
+    </section>
+
+   );
 }
  
 export default Home;

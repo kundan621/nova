35614470b60f89be36f7fd77a10c83c4ce0e2a5a
Haaris Shamim <shaaris484@gmail.com>
2024-01-17 01:49:00 +0530

    included post method

--- app/api/intro/route.js
+++ app/api/intro/route.js
@@ -1,4 +1,5 @@
 //http://localhost:3000/api/intro
+import { NextResponse } from "next/server";
 
 export async function GET(request){
     const users=[
@@ -9,3 +10,13 @@ export async function GET(request){
 
     return new Response(JSON.stringify(users))
 }
+export async function POST(req, res) {
+    try {
+        const data = await req.json();  // Parse the JSON body
+        console.log('Received data:', data);
+        return NextResponse.json({ message: 'Hello from the endpoint!' })
+      } catch (error) {
+        console.error('Error processing request:', error);
+            return NextResponse.json({ message: 'Hello from the endpoint!' })
+        }
+    }

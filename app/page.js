aff176c8863ed4dd586616071f373bf0a9a0c1c8
Kundan Kumar <kundan621@gmail.com>
2024-01-18 02:28:34 +0530

    added flex in home

--- app/page.js
+++ app/page.js
@@ -1,19 +1,58 @@
 const Home = () => {
-  return ( 
-    <section className="w-full flex-center flex-col">
-      <h1 className="head_text text-center">
-        HK Software
-        <br className="max-md:hidden"/>
-        <span className="orange_gradient text-center"> Consulting pro Max</span>
-      </h1>
+  return (
+    <>
+      <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+          HK Software
+          <br className="max-md:hidden" />
+          <span className="orange_gradient text-center">
+            {" "}
+            Consulting pro Max
+          </span>
+        </h1>
 
-      <p className="desc text-center">
-        HK Software is a one stop solution for building scalable products. We make products which brings fortune and helps people solve their problems.
-      </p>
+        <p className="desc text-center">
+          HK Software is a one stop solution for building scalable products. We
+          make products which brings fortune and helps people solve their
+          problems.
+        </p>
+      </section>
 
-    </section>
+      <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+          HK Software
+          <br className="max-md:hidden" />
+          <span className="orange_gradient text-center">
+            {" "}
+            Consulting pro Max
+          </span>
+        </h1>
 
-   );
-}
- 
+        <p className="desc text-center">
+          HK Software is a one stop solution for building scalable products. We
+          make products which brings fortune and helps people solve their
+          problems.
+        </p>
+      </section>
+
+      <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+          HK Software
+          <br className="max-md:hidden" />
+          <span className="orange_gradient text-center">
+            {" "}
+            Consulting pro Max
+          </span>
+        </h1>
+
+        <p className="desc text-center">
+          HK Software is a one stop solution for building scalable products. We
+          make products which brings fortune and helps people solve their
+          problems.
+        </p>
+      </section>
+    </>
+  );
+};
+
 export default Home;
